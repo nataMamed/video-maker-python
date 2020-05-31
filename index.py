d2cba980ea19e785dda8bcc5b9370dcd2bcbce20
Natã Mamede <natamamede@gmail.com>
2020-05-31 15:50:53 -0300

    feature: resotoring the pipevn enviroment

--- index.py
+++ index.py
@@ -5,19 +5,17 @@ class VideoMakerPython:
 
 
     def __init__(self):
-        
-
+    
         self.user_input = user_input()
         self.text = Text(self.user_input['searchTerm'],7)
 
     def start(self):
 
-        
         robots = {
-            'userInput': self.user_input,
-            'sourceContentOriginal': self.text.content,
-            'souceContentSanitized' : self.text.sanitized_content,
-            'sentences': self.text.break_into_sentences()
+            "userInput": self.user_input,
+            "sourceContentOriginal": self.text.content,
+            "souceContentSanitized" : self.text.sanitized_content,
+            "sentences": self.text.break_into_sentences()
         }
         
         return robots
